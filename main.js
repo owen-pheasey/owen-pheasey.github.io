679b4679920b8d30233a627ed0f6e80e586469f5
owen pheasey <owen.pheasey@gmail.com>
2017-03-12 20:40:02 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var myName = "Owen Pheasey";
+
+var red = [10, 190, 623];
+var orange = [40, 76, 63];
+var green = [75, 100, 623];
+var blue = [196, 76, 63];
+var purple = [280, 50, 623];
+var another = [320, 76, 63]
+var letterColors = [another, red, orange, green, blue, purple];
+
+drawName(myName, letterColors);
+
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'square';
+}
+
+bounceBubbles();
